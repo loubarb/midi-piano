1a56c68debbcc2411d9f481af98995c0965dcde0
loubarb <louis.barbati@gmail.com>
2022-05-28 22:06:37 -0400

    Added frequency sounds to corresponding keys

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const audioContext = new AudioContext()
+
 const NOTE_DETAILS = [
   { note: "C", key: "Z", frequency: 261.626 },
   { note: "Db", key: "S", frequency: 277.183 },
@@ -17,7 +19,7 @@ document.addEventListener('keydown', (e) => {
   if (e.repeat === true) return
   const keyCode = e.code
   const noteDetail = getNoteDetail(keyCode)
-  if (noteDetail === null) return
+  if (noteDetail == null) return
   noteDetail.active = true
   playNotes()
 })
@@ -25,7 +27,7 @@ document.addEventListener('keydown', (e) => {
 document.addEventListener('keyup', (e) => {
   const keyCode = e.code
   const noteDetail = getNoteDetail(keyCode)
-  if (noteDetail === null) return
+  if (noteDetail == null) return
   noteDetail.active = false
   playNotes()
 })
@@ -38,5 +40,26 @@ function playNotes() {
   NOTE_DETAILS.forEach((n) => {
     const keyElement = document.querySelector(`[data-note="${n.note}"]`)
     keyElement.classList.toggle('active', n.active || false)
+    if (n.oscillator != null) {
+      n.oscillator.stop()
+      n.oscillator.disconnect()
+    }
   })
+
+  const activeNotes = NOTE_DETAILS.filter(n => n.active)
+  const gain = 1 / activeNotes.length
+  activeNotes.forEach((n) => {
+    startNote(n, gain)
+  })
+}
+
+function startNote(noteDetail, gain) {
+  const gainNode = audioContext.createGain()
+  gainNode.gain.value = gain
+  const oscillator = audioContext.createOscillator()
+  oscillator.frequency.value = noteDetail.frequency
+  oscillator.type = 'triangle'
+  oscillator.connect(gainNode).connect(audioContext.destination)
+  oscillator.start()
+  noteDetail.oscillator = oscillator
 }
